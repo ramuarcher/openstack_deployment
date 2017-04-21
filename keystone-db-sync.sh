ae4e994d300c325e32f22c9fe5bc2df3169189a5
ramu.nakerikanti <ramu.nakerikanti@tiss.edu>
2017-04-21 15:20:39 +0530

    keystone db-sync issue need to resolve

---
+++ keystone-db-sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+su -s /bin/bash keystone -c "keystone-manage db_sync"
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+export controller=192.168.122.34
+keystone-manage bootstrap --bootstrap-password adminpassword --bootstrap-admin-url http://$controller:35357/v3/ --bootstrap-internal-url http://$controller:35357/v3/ --bootstrap-public-url http://$controller:5000/v3/ --bootstrap-region-id RegionOne
+      
